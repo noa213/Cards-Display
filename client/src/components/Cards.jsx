92bdc3072be517dfbff7b617a5cd70e56376d899
noa <abmagori@gmail.com>
2024-11-03 10:19:53 +0200

    last commit

--- client/src/components/Cards.jsx
+++ client/src/components/Cards.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import axios from "axios";
 import { useEffect, useState } from "react";
-// import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import Card from "./Card";
 
 const Cards = () => {
@@ -28,64 +27,6 @@ const Cards = () => {
     setCards(newData);
   };
 
-  //   const reorder = (list, startIndex, endIndex) => {
-  //     const result = Array.from(list);
-  //     const [removed] = result.splice(startIndex, 1);
-  //     result.insert(endIndex, removed);
-  //     return result;
-  //   };
-
-//   const onDragEnd = (result) => {
-//     // Check if the drag was successful
-//     if (!result.destination) return;
-
-//     // Get the old and new indexes of the dragged card
-//     const oldIndex = result.source.index;
-//     const newIndex = result.destination.index;
-
-//     // Check if the card was moved within the same list
-//     if (result.source.droppableId === result.destination.droppableId) {
-//       // Reorder the cards locally using a copy of the state
-//       const newCards = [...cards];
-//       const [removed] = newCards.splice(oldIndex, 1);
-//       newCards.splice(newIndex, 0, removed);
-//       setCards(newCards);
-//     } else {
-//       // Handle card movement between different lists (future implementation)
-//       console.warn("Moving cards between lists not implemented yet.");
-//     }
-//   };
-  //   const Grid = (cards) => {
-  //     return (
-  //       <DragDropContext onDragEnd={onDragEnd}>
-  //         <Droppable droppableId="droppable">
-  //           {(provided) => (
-  //             <div {...provided.droppableProps} ref={provided.innerRef}>
-  //               {cards.map((card, index) => (
-  //                 <Draggable key={card.id} draggableId={card.id} index={index}>
-  //                   {(provided) => (
-  //                     <div ref={provided.innerRef} {...provided.draggableProps}>
-  //                       <div
-  //                         {...provided.dragHandleProps}
-  //                         style={{ cursor: "grab" }}
-  //                       >
-  //                         {'/* Content for the drag handle */'}
-  //                       </div>
-  //                       <Card key={card.id} onUpdate={handleUpdate} card={card} />
-  //                     </div>
-  //                   )}
-  //                 </Draggable>
-  //               ))}
-  //               {provided.placeholder}
-  //               <button className="add-btn" onClick={addCard}>
-  //                 +
-  //               </button>
-  //             </div>
-  //           )}
-  //         </Droppable>
-  //       </DragDropContext>
-  //     );
-  //   };
   return (
     <div>
       <h1>CARDS</h1>
